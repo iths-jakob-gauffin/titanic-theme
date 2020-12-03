224f819940e199cea4f4f2ec33f3924ebec89c00
iths-suriyan-schock <54362647+iths-suriyan-schock@users.noreply.github.com>
2020-12-03 12:38:23 +0100

    Första commit

--- themes/titanictheme/index.php
+++ themes/titanictheme/index.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 
-    <h1>hejsan det här den första texten, en h1:a</h1>
+    <h1>hejsan det här den första textenenenen, en h1:a</h1>
     <button class="knapp">klicka</button>
 
 <?php get_footer(); ?>
