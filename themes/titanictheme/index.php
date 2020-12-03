03cb466abee612c2064489043efc1ed3e80477f4
iths-jennifer-bunner <jenniferbunner@hotmail.com>
2020-12-03 13:03:02 +0100

    third commit

--- themes/titanictheme/index.php
+++ themes/titanictheme/index.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 
-    <h1>hejsan det här den första texten hej, en h1:a</h1>
+    <h1>hejsan det här den första texten heeeej, en h1:a</h1>
     <button class="knapp">klicka</button>
 
 <?php get_footer(); ?>
