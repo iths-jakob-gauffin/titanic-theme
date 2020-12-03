8af4f7f9659b0ee092591d48bd9d17d4d5f7716f
iths-jakob-gauffin <54362410+iths-jakob-gauffin@users.noreply.github.com>
2020-12-03 14:33:06 +0100

    Fix to push to jakob branch

--- themes/titanictheme/index.php
+++ themes/titanictheme/index.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 
-    <h1>hejsan det här den första texten, en h1:a</h1>
+    <h1>hejsan det här den första texten, en h1:a. Text bara för att pusha till jakobbranchen.</h1>
     <button class="knapp">klicka</button>
 
 <?php get_footer(); ?>
