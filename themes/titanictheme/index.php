24b07c6dc3b3397e5199afef391782da7f1c29a3
iths-jennifer-bunner <jenniferbunner@hotmail.com>
2021-01-14 14:05:11 +0100

    added headers in variables.scss

--- themes/titanictheme/index.php
+++ themes/titanictheme/index.php
@@ -1,5 +1,24 @@
 <?php get_header(); 
+        $harbors = new WP_Query(array(
+            'post_type' => 'harbor'
+        ));
     
+        $harborsArray = $harbors->posts;
+        // jag vill ta fram posten med samma post_title som sessionsvariabeln, för att komma åt den postens thumbnail
+    
+        $imgUrl = "";
+    
+        foreach ($harborsArray as $value) {
+            // echo var_dump($_SESSION['hamn']);
+            // echo var_dump($value->post_title);
+            if(strtolower($_SESSION['hamn']) === strtolower($value->post_title)){
+                // echo var_dump($value);
+                $stuff = get_post_thumbnail_id( $value->ID );
+                // echo var_dump(get_the_post_thumbnail(  ));
+                $imgUrl = get_the_post_thumbnail_url($value->ID, "backgroundImage");
+            }
+        }
+        // echo var_dump($imgUrl);
 
     global $query_string;
     // echo var_dump($query_string);
@@ -9,7 +28,17 @@
     $search = new WP_Query( $search_query );
     ?>
     
-    <h1>Det här är Index.php</h1>
+    <h1>Det hääär är Index.php</h1>
+    <!-- --------Header-------- -->
+    <div class="Blog">
+        <div class="Blog__BackgroundImage" style="background: url('<?php echo $imgUrl; ?>')">
+        ¨<div class="Blog__TitleWrapper">
+            <h1 class="Blog__Title">
+               Blogg
+            </h1>
+        </div>
+    </div>
+<!-- --------Header slut---------- -->
 
     <?php
 
@@ -25,6 +54,12 @@
             <main class="Blog">
                 <article class="Blog__Post">
                     <a href="<?php the_permalink(); ?>" class="Blog__PostLink"></a>
+
+
+                    <!-- här gör vi en container, bestäm storlek i scccen-->
+                    <div class="Blog__ImageContainer">
+                        <img src="<?php echo get_the_post_thumbnail_url();?>" class="Blog__Image">
+                    </div>
                         <h2 class="Blog__PostTitle">
                             <?php the_title(); ?>
                         </h2>
@@ -32,7 +67,7 @@
                         <?php 
                             
                             $text = get_the_content();
-                            echo wp_trim_words($text, 5);
+                            echo wp_trim_words($text, 15);
                             
                             ?>
                     </div>
